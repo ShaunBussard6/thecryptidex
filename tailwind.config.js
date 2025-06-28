4b596650c0831fe0cc7a8794a90b0cb051e7fb9c
shaun bussard <shaunbussard@shaunbussard2021s-iMac.local>
2025-06-27 18:52:20 -0600

    Run clean build, ready for Netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './app/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
     },
-    plugins: [require("tw-animate-css")],
-  }
-  
+  },
+  plugins: [],
+}
